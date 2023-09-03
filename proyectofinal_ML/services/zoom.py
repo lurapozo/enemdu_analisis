6775dda3fbbb580057c2a1f2acf89c0e7d1fe941
AnthonyTerreros <dterrero@espol.edu.ec>
2023-09-03 11:58:01 -0500

    correcion de zoom

--- proyectofinal_ML/services/zoom.py
+++ proyectofinal_ML/services/zoom.py
@@ -1,13 +1,18 @@
 import pyautogui as py
 import time
-from selenium import webbrowser
 
-driver = webbrowser.Chrome()
+from selenium import webdriver
+from selenium.webdriver.common.alert import Alert
+
+driver = webdriver.Chrome()
 
 def connect_zoom(meet_code, password):
     # Example = https://us04web.zoom.us/j/77943644084?pwd=TWI0WS91b3dqUG1Jd1hUQkFzYTh0QT09
-    driver.open(f"https://zoom.us/j/{meet_code}?pwd={password}")
-    driver.switch_to.alert.accept()
+    driver.get(f"https://zoom.us/j/{meet_code}?pwd={password}")
+    driver.maximize_window()
+    time.sleep(2)
+    alert = Alert(driver)
+    alert.dismiss()
     # Cambiar el tiempo de espera dependiendo del rendimiento de tu maquina.
     time.sleep(8)
     py.press('c')
